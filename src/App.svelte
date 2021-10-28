27e2f4c769cc55ad4cb25cfbb24dbbea8a6c1f66
JaydenDev <92550746+JaydenDev@users.noreply.github.com>
2021-10-28 17:53:36 -0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,23 +2,10 @@
   import windowslist from "./data";
   import themelist from "./themelist";
   import Notifications from "svelte-notifications";
-  import Child from "./child.svelte";
   let selectedTheme = "w11dark";
 </script>
 
 <div class="container {selectedTheme}">
-  <div class="notification">
-    <Notifications>
-      <Child {selectedTheme}/>
-    </Notifications>
-  </div>
-  
-  <label for="theme">Choose a theme: </label>
-  <select name="theme" id="theme" bind:value={selectedTheme}>
-    {#each themelist as theme}
-      <option value={theme}>{theme}</option>
-    {/each}
-  </select>
   <h1>WinInfo by JaydenDevelopment</h1>
   <p>Dynamic WebApp with information about Windows builds.</p>
   <div class="card {selectedTheme}">
@@ -40,6 +27,12 @@
   </table>
   </div>
 </div>
+<label for="theme">Choose a theme: </label>
+<select name="theme" id="theme" bind:value={selectedTheme}>
+  {#each themelist as theme}
+    <option value={theme}>{theme}</option>
+  {/each}
+</select>
 
 <style>
   /* button {
@@ -76,7 +69,7 @@
   .card {
     font-size: 12pt;
     height: 50%;
-    width: 35%;
+    width: 85%;
     background-color: white;
     border-radius: 5pt;
     color: black;
