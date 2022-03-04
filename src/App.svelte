3e5bad3bae2121126ebc926c590d696f0b628738
JaydenDev <92550746+JaydenDev@users.noreply.github.com>
2022-03-04 14:36:26 -0600

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script>
-  import osListImport from "https://jaydendev.github.io/remote/osinfo/dat/data";
+  import osListImport from "https://raw.githubusercontent.com/JaydenDev/OSInfo-Data/main/data.js";
   let selectedTheme = "w11dark";
 
   let oslist = osListImport;
